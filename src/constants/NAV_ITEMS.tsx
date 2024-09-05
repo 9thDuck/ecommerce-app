e8f5f88ce87a58cbe1b6d80145ad7d27b37eea1b
7thclass <msai@live.co.uk>
2024-09-05 16:21:20 +0530

    nav items

---
+++ src/constants/NAV_ITEMS.tsx
@@ -0,0 +1,18 @@
+export const NAV_ITEMS = [
+    {
+        name: "Home",
+        path: "/",
+    },
+    {
+        name: "Products",
+        path: "/products",
+    },
+    {
+        name: "Contact",
+        path: "/contact",
+    },
+    {
+        name: "About",
+        path: "/about",
+    },
+]
