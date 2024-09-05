4a7e27e34d0440d8df47904ba059cad5c794eefe
Mylavarapu Sarath Sai <msai@live.co.uk>
2024-09-05 22:20:33 +0530

    dummy products data

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,3 +1,4 @@
 export { NAV_ITEMS } from "./NAV_ITEMS";
 export { GLOBE_CONFIG, GLOBE_SAMPLE_ARCS } from "./HERO_GLOBE.config";
+export { PRODUCTS } from "./PRODUCTS";
 export { ABOUT_PAGE_CONTENT } from "./ABOUT_PAGE_CONTENT";
