f653a2ee26e0dcb5a330aea8ab16e72d34ed7faa
7thclass <msai@live.co.uk>
2024-09-05 16:48:22 +0530

    initial home page component

---
+++ src/pages/home.tsx
@@ -0,0 +1,11 @@
+import PageContent from "../components/page-content"
+
+const Home = () => {
+    return (
+        <PageContent>
+            <h1>HomePage</h1>
+        </PageContent>
+    )
+}
+
+export default Home
