c22d40f5b721533b5c04090a9aabde30d3ac0d0d
Mylavarapu Sarath Sai <msai@live.co.uk>
2024-09-06 12:13:37 +0530

    added minor padding

--- src/components/page-content.tsx
+++ src/components/page-content.tsx
@@ -1,9 +1,5 @@
 const PageContent = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <main className="flex-grow container mx-auto">
-            {children}
-        </main>
-    )
-}
+  return <main className="flex-grow container mx-auto p-4">{children}</main>;
+};
 
-export default PageContent
+export default PageContent;
