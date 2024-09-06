135115e95cb88ff71e6d969d326fa0588ab3862d
Mylavarapu Sarath Sai <msai@live.co.uk>
2024-09-06 12:47:55 +0530

    added router provider to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { RouterProvider } from "react-router-dom"
-import { router } from "./router"
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 const App = () => {
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  );
+};
 
-    return (
-        <RouterProvider router={router} />
-    )
-}
-
-export default App
+export default App;
