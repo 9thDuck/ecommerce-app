fc47a788ea1ac3942e6f07217fb4e5b646255466
Mylavarapu Sarath Sai <msai@live.co.uk>
2024-09-06 16:07:00 +0530

    styling changes and refactored empty cart component to a seperate component

--- src/pages/cart/cart.tsx
+++ src/pages/cart/cart.tsx
@@ -58,8 +58,25 @@ const Cart = () => {
   const handleGenerateDiscountCoupon = () => {
     // Hardcoded for now, in order to check the discount coupon feature.
     // Will become dynamic once the orders page is implemented.
-    dispatch(generateDiscountCoupon(4));
+    dispatch(generateDiscountCoupon(1));
     setAlreadyCheckedForDiscountCoupon(true);
+  };
+
+  const EmptyCart = () => {
+    return (
+      <div className="text-center py-12">
+        <ShoppingCart className="mx-auto h-24 w-24 text-gray-400 mb-4" />
+        <h2 className="text-2xl font-semibold text-gray-600 mb-2">
+          Your cart is empty
+        </h2>
+        <p className="text-gray-500 mb-6">
+          Looks like you haven't added anything to your cart yet.
+        </p>
+        <Button asChild>
+          <Link to="/products">Start Shopping</Link>
+        </Button>
+      </div>
+    );
   };
 
   const handleApplyDiscount = () => {
@@ -71,27 +88,16 @@ const Cart = () => {
       <div className="container mx-auto p-4">
         <h1 className="text-3xl font-bold mb-4">Your Cart</h1>
         {items.length === 0 ? (
-          <div className="text-center py-12">
-            <ShoppingCart className="mx-auto h-24 w-24 text-gray-400 mb-4" />
-            <h2 className="text-2xl font-semibold text-gray-600 mb-2">
-              Your cart is empty
-            </h2>
-            <p className="text-gray-500 mb-6">
-              Looks like you haven't added anything to your cart yet.
-            </p>
-            <Button asChild>
-              <Link to="/products">Start Shopping</Link>
-            </Button>
-          </div>
+          <EmptyCart />
         ) : (
           <div>
             <ul className="space-y-4">
               {items.map((item) => (
                 <li
                   key={item.id}
-                  className="flex justify-between items-center p-4 border rounded-lg shadow-sm"
+                  className="flex justify-between p-4 border rounded-lg shadow-sm items-start "
                 >
-                  <div>
+                  <div className="flex flex-col gap-2">
                     <h2 className="text-xl font-semibold">{item.title}</h2>
                     <p className="text-gray-600">Price: ${item.price}</p>
                     <p className="text-gray-600">Quantity: {item.quantity}</p>
